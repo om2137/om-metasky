8a5a62120a75ac21095b4bcd41a8064bc47035e5
om2137 <omraut961@gmail.com>
2023-12-19 20:20:35 +0530

    redux store

---
+++ src/store.ts
@@ -0,0 +1,25 @@
+import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './slices/reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, 
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
